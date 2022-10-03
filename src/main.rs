4fcd3a5dd94d4498fa02a191edce8998744f534a
tobyw <tobywang21@outlook.com>
2022-10-03 15:59:26 +1100

    finished fft1d

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
+#![allow(unused)]
+
 mod fft;
 
 use fft::{read_img, fft_2d};
-
 use std::env::args;
 
 fn main() {
+
+    
     let argv: Vec<String> = args().collect();
 
+    // load image and pipe into fft2d
     let mut img = read_img(&argv[1]);
     fft_2d(&mut img);
-
+    
 }
+
