219a3d628725c70123efb436db2214e2a1d08551
Toby Wang <tobywang21@outlook.com>
2025-08-26 23:05:50 +1000

    [update] update display

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,38 @@
 
 mod fft;
 
-use fft::{read_img, fft_2d};
-use std::env::args;
 use clap::Parser;
+use fft::{fft_2d, read_img};
+use num::complex::Complex64;
+use std::{env::args, process::exit};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-
     #[arg(short, long)]
     image: String,
 
     #[arg(short, long)]
-    watermark: String
-
+    watermark: String,
 }
 
 fn main() {
-
     let args = Args::parse();
 
-
     // load image and pipe into fft2d
-    let mut img = read_img(&args.image);
-    let mut watermark = read_img(&args.watermark);
+    let mut img = match read_img(&args.image) {
+        Ok(img) => img,
+        Err(e) => {
+            eprintln!("unable to open image: {}", e);
+            exit(1);
+        }
+    };
+    let mut watermark = match read_img(&args.watermark) {
+        Ok(watermark) => watermark,
+        Err(e) => {
+            eprintln!("unable to open watermark: {}", e);
+            exit(1);
+        }
+    };
     fft_2d(img, watermark);
-
-    
 }
-
